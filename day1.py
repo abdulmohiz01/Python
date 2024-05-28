98381eaf58fe496eaac9f62811a0e14012e5ca58
Abdul Mohiz <skylark7768@gmail.com>
2024-05-28 17:29:32 +0500

    Day 1- Learned how to print, take value from a user, len(), Variable, String Concatenation.

---
+++ day1.py
@@ -0,0 +1,20 @@
+# print("Hello World!")
+# print("String " + "Concatenation")
+# print("newline\n")
+# print('String concatenation is done with "+" sign')
+# name = input("What's your Name?")
+#len(), input(), #print()
+# print("Hello " + input("What is your Name?"))
+# print(len(input("Your name\n")))
+# n = "Almohiz"
+# a = input("Enter value for a: ")
+# b= input("Enter value for b:")
+# c=a
+# a=b
+# b=c
+# print("a= " + a)
+# print("b= " + b)
+print("Welcome to the Band Name Generator.")
+city = input("In which city you grew in?")
+pet = input("Enter your pet's Name")
+print("Your Band Name is " + city+pet)
